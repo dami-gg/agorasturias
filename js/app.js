86f8c27030497c65f94a9042a48cf0f96677f06d
Dami <damian.garciagarcia@gmail.com>
2014-10-26 13:56:05 +0100

    Added pagination, improved carousel and header for small screens

--- js/app.js
+++ js/app.js
@@ -49,7 +49,7 @@ agorasturiasApp.config(
 
 agorasturiasApp.controller('CarouselCtrl', function ($scope) {
 
-    $scope.myInterval = 5000;
+    $scope.interval = 5000;
     var slides = $scope.slides = [];
   
     $scope.addSlide = function() {
@@ -67,6 +67,40 @@ agorasturiasApp.controller('CarouselCtrl', function ($scope) {
     }
 });
 
+agorasturiasApp.controller('PostsCtrl', function ($scope) {
+
+    var posts = $scope.posts = [];
+
+    $scope.addPost = function() {
+        posts.push({
+          title: 'AUTUMN AGORA IS CALLING',
+          image: 'http://www.europarl.it/resource/static/images/martin_schulz_ep_president_1.jpg',
+          text: ['On the road to Autumn Agora Cagliari 2014...', 'Summer has finally come but nothing could ever stop preparations for the AEGEE Autumn Agora 2014, one of the most important youth events in Europe in terms of number of participants and importance of the discussed issues: thanks to this, AEGEE has obtained a role in the United Nations advisory board and in the Council of Europe.','Agora is the general Assembly of AEGEE association and it takes place twice a year, every year, since 1986, involving generations of students from hundreds of cities. In this special occasion, all of the European Antennae reach the organizing city of the event in order to deal with topics that concern active citizenship and the impact of young people in Europe of the future.']
+        });
+    };
+
+    for (var i=0; i<7; ++i) {
+        $scope.addPost();
+    }
+
+    $scope.totalItems = posts.length;
+    $scope.currentPage = 1;
+    $scope.itemsPerPage = 5;
+
+    $scope.setPage = function (pageNumber) {
+        $scope.currentPage = pageNumber;
+    };
+
+    $scope.pageChanged = function() {
+        var begin = (($scope.currentPage - 1) * $scope.itemsPerPage);
+        var end = begin + $scope.itemsPerPage;
+
+        $scope.pagedPosts = $scope.posts.slice(begin, end);
+    };
+
+    $scope.pageChanged();
+});
+
 agorasturiasApp.controller('FormCtrl', function ($scope) {
 
     $scope.submitForm = function(isValid) {
@@ -76,5 +110,4 @@ agorasturiasApp.controller('FormCtrl', function ($scope) {
         }
 
     };
-
 });
