c815377987b3e726798cb2d2e92f301108ee8446
Dami <damian.garciagarcia@gmail.com>
2014-11-30 17:17:04 +0100

    Added pdf slider

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 // create the module including ngRoute for all the routing needs
 var agorasturiasApp = angular.module('agorasturiasApp', ['ui.router', 'ui.bootstrap']);
+// , 'uiRouterStyles'
 
 // configure the routes
 agorasturiasApp.config(
@@ -25,9 +26,22 @@ agorasturiasApp.config(
                 templateUrl : 'pages/aegee-europe.html'
             })
 
-            .state('agora', {
-                url : '/agora',
-                templateUrl : 'pages/agora.html'
+            .state('agora-for-dummies', {
+                url : '/agora-for-dummies',
+                templateUrl : 'pages/agora-for-dummies.html'/*,
+                data : {
+                    css : 'css/wallop-slider.css'
+                }*/
+            })
+
+            .state('event-timetable', {
+                url : '/event-timetable',
+                templateUrl : 'pages/event-timetable.html'
+            })
+
+            .state('pre-events', {
+                url : '/pre-events',
+                templateUrl : 'pages/pre-events.html'
             })
 
             .state('info', {
@@ -109,5 +123,133 @@ agorasturiasApp.controller('FormCtrl', function ($scope) {
             alert('our form is amazing');
         }
 
+    };
+});
+
+agorasturiasApp.controller('BookCtrl', function ($scope) {
+
+  $scope.index = 0;
+  
+  $scope.images = [
+    'img/agora-for-dummies-page-1.jpg',
+    'img/agora-for-dummies-page-2.jpg',
+    'img/agora-for-dummies-page-3.jpg',
+    'img/agora-for-dummies-page-4.jpg',
+    'img/agora-for-dummies-page-5.jpg',
+    'img/agora-for-dummies-page-6.jpg',
+    'img/agora-for-dummies-page-7.jpg',
+    'img/agora-for-dummies-page-8.jpg',
+    'img/agora-for-dummies-page-9.jpg',
+    'img/agora-for-dummies-page-10.jpg',
+    'img/agora-for-dummies-page-11.jpg',
+  ];
+
+}).directive('wallopSlider', function () {
+    return {
+      template: '<div class="wallop-slider {{animationClass}}"><ul class="wallop-slider__list"><li class="wallop-slider__item {{itemClasses[$index]}}" ng-repeat="i in images"><img ng-src="{{i}}"></li></ul><button ng-show="images.length>1" class="st-button wallop-slider__btn wallop-slider__btn--previous btn btn-success btn--previous" ng-disabled="prevDisabled" ng-click="onPrevButtonClicked()">Previous</button><button ng-show="images.length>1" class="st-button wallop-slider__btn wallop-slider__btn--next btn btn-success btn--next" ng-disabled="nextDisabled" ng-click="onNextButtonClicked()">Next</button></div>',
+      restrict: 'EA',
+      transclude: true,
+      replace: false,
+      scope: {
+        images: '=',
+        animation: '@',
+        currentItemIndex: '=',
+        onNext: '&',
+        onPrevious: '&'
+      },
+      controller: function($scope, $timeout) {
+
+        $scope.itemClasses = [];
+
+        $scope.$watch('images', function(images) {
+          if (images.length) {
+            _goTo(0);
+          }
+        });
+
+        // set animation class corresponding to animation defined in CSS. e.g. rotate, slide
+        if ($scope.animation) {
+          $scope.animationClass = 'wallop-slider--' + $scope.animation;
+        }
+
+        var _displayOptions = {
+          btnPreviousClass: 'wallop-slider__btn--previous',
+          btnNextClass: 'wallop-slider__btn--next',
+          itemClass: 'wallop-slider__item',
+          currentItemClass: 'wallop-slider__item--current',
+          showPreviousClass: 'wallop-slider__item--show-previous',
+          showNextClass: 'wallop-slider__item--show-next',
+          hidePreviousClass: 'wallop-slider__item--hide-previous',
+          hideNextClass: 'wallop-slider__item--hide-next'
+        };
+
+        function updateClasses() {
+          if ($scope.itemClasses.length !== $scope.images.length) {
+            $scope.itemClasses = [];
+            for (var i=0; i<$scope.images.length; i++) {
+              $scope.itemClasses.push('');
+            }
+          }
+        }
+        function _nextDisabled() {
+          return ($scope.currentItemIndex + 1) === $scope.images.length;
+        }
+        function _prevDisabled() {
+          return !$scope.currentItemIndex;
+        }
+        function _updatePagination() {
+          $scope.nextDisabled = _nextDisabled();
+          $scope.prevDisabled = _prevDisabled();
+        }
+        function _clearClasses() {
+          for (var i=0; i<$scope.images.length; i++) {
+            $scope.itemClasses[i] = '';
+          }
+        }
+
+        // go to slide
+        function _goTo(index) {
+          if (index >= $scope.images.length || index < 0 || index === $scope.currentItemIndex) {
+
+            if (!index) {
+              $scope.itemClasses[0] = _displayOptions.currentItemClass;
+            }
+            return;
+          }
+
+          _clearClasses();
+
+          $scope.itemClasses[$scope.currentItemIndex] = (index > $scope.currentItemIndex) ? _displayOptions.hidePreviousClass : _displayOptions.hideNextClass;
+
+          var currentClass = (index > $scope.currentItemIndex) ? _displayOptions.showNextClass : _displayOptions.showPreviousClass;
+          $scope.itemClasses[index] = _displayOptions.currentItemClass + ' ' + currentClass;
+
+          $scope.currentItemIndex = index;
+
+          _updatePagination();
+
+        }
+
+        // button event handlers
+        $scope.onPrevButtonClicked = function () {
+          _goTo($scope.currentItemIndex - 1);
+        };
+        $scope.onNextButtonClicked = function () {
+          _goTo($scope.currentItemIndex + 1);
+        };
+        
+        $scope.$watch('currentItemIndex', function(newVal, oldVal) {
+          if (oldVal > newVal) {
+            if (typeof $scope.onPrevious === 'function') {
+              $scope.onPrevious();
+            }
+          } else {
+            if (typeof $scope.onNext === 'function') {
+              $scope.onNext();
+            }
+          }
+        });
+
+      }
     };
 });
