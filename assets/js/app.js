c7aea0c7120b5d9bb6e738ebb3b61ae4880a0fec
System Administrator <root@MacBook-de-Dami.local>
2014-12-20 13:03:55 +0100

    Added core team thumbnails

--- assets/js/app.js
+++ assets/js/app.js
@@ -273,3 +273,36 @@ agorasturiasApp.controller('PartnersCtrl', function ($scope) {
         $scope.addPartner();
     }
 });
+
+agorasturiasApp.controller('ThumbnailsCtrl', function ($scope, partitionService) {
+
+  var members = $scope.members = [];
+
+  $scope.addMember = function() {
+    members.push({
+      name: 'Alberto Cuesta',
+      position: 'Main organizer',
+      image: 'http://goo.gl/eJvm24',
+      hover: 'http://goo.gl/y2tsTX'
+    });
+  };
+
+  for (var i=0; i<7; ++i) {
+    $scope.addMember();
+  }
+
+  $scope.rows = partitionService.partition(members, 4);
+});
+
+agorasturiasApp.service('partitionService', function() {
+
+  this.partition = function (dataArray, chunkSize) {
+    var result = [];
+
+    for (var i = 0; i < dataArray.length; i += chunkSize) {
+      result.push(dataArray.slice(i, i+chunkSize));
+    }
+
+    return result;
+  };
+});
