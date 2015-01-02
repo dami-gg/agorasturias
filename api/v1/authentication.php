8c42b980e4dc974563da73939ca9d8f831a20593
albertocuesta <albertocuesta@gmail.com>
2015-01-02 13:49:12 +0100

    added all php libraries

---
+++ api/v1/authentication.php
@@ -0,0 +1,57 @@
+<?php
+/*
+TODO:Make authentication secure
+*/
+$app->get('/session', function() {
+  $db = new DbHandler();
+  $session = $db->getSession();
+  $response["uid"] = $session['uid'];
+  $response["email"] = $session['email'];
+  $response["name"] = $session['name'];
+  echoResponse(200, json_encode($session));
+});
+
+$app->post('/login', function() use ($app) {
+  require_once 'passwordHash.php';
+  $r = json_decode($app->request->getBody());
+  verifyRequiredParams(array('username', 'password'),$r);
+  $response = array();
+  $db = new DbHandler();
+  // $password = $r->user->password;
+  // $username = $r->user->username;
+  $password = $r->password;
+  $username = $r->username;
+  $user = $db->getOneRecord("select uid,username,name,password,email,app_id from users where username='$username'");
+  if ($user != NULL) {
+    if(passwordHash::check_password($user['password'],$password)){
+      $response['status'] = "success";
+      $response['message'] = 'Logged in successfully.';
+      $response['name'] = $user['name'];
+      $response['uid'] = $user['uid'];
+      $response['email'] = $user['email'];
+      $response['app_id'] = $user['app_id'];
+      $response['username'] = $user['username'];
+      if (!isset($_SESSION)) {
+        session_start();
+      }
+    }
+    else {
+      $response['status'] = "error";
+      $response['message'] = 'Login failed. Incorrect credentials';
+    }
+  }
+  else {
+    $response['status'] = "error";
+    $response['message'] = 'No such user is registered';
+  }
+  echoResponse(200, $response);
+});
+
+$app->get('/logout', function() {
+  $db = new DbHandler();
+  $session = $db->destroySession();
+  $response["status"] = "info";
+  $response["message"] = "Logged out successfully";
+  echoResponse(200, $response);
+});
+?>
---
+++ api/v1/index.php
@@ -0,0 +1,57 @@
+<?php
+
+require_once 'dbHandler.php';
+require_once 'passwordHash.php';
+require '.././libs/Slim/Slim.php';
+
+\Slim\Slim::registerAutoloader();
+
+$app = new \Slim\Slim();
+
+// User id from db - Global Variable
+$user_id = NULL;
+
+require_once 'authentication.php';
+require_once 'posts.php';
+
+/**
+* Verifying required params posted or not
+*/
+function verifyRequiredParams($required_fields,$request_params) {
+  $error = false;
+  $error_fields = "";
+  foreach ($required_fields as $field) {
+    if (!isset($request_params->$field) || strlen(trim($request_params->$field)) <= 0) {
+      $error = true;
+      $error_fields .= $field . ', ';
+    }
+  }
+  if ($error) {
+    // Required field(s) are missing or empty
+    // echo error json and stop the app
+    $response = array();
+    $app = \Slim\Slim::getInstance();
+    $response["input"] = $request_params;
+    $response["status"] = "error";
+    $response["message"] = 'Required field(s) ' . substr($error_fields, 0, -2) . ' is missing or empty';
+    echoResponse(200, $response);
+    $app->stop();
+  }
+}
+
+
+function echoResponse($status_code, $response) {
+  $app = \Slim\Slim::getInstance();
+  // Http response code
+  $app->response->status($status_code);
+
+  // setting response content type to json
+  $app->response->headers->set('contentType','application/json');
+
+  $json = json_encode($response);
+
+  echo $json;
+}
+
+$app->run();
+?>
