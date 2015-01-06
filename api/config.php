4e7f2a7ac336fa0e9e642da0ae49321aef2bfe96
Dami <damian.garciagarcia@gmail.com>
2015-01-06 20:46:09 +0100

    Changed dropdown for logged-in users

--- api/config.php
+++ api/config.php
@@ -3,7 +3,7 @@
 * Update before moving to production
 */
 define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_PASS', 'root');
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'agorasturias');
 
