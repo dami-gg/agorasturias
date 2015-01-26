1ce9bfb9d0feb819b0c9fc42fc5e38e36a8e9d33
Dami <damian.garciagarcia@gmail.com>
2015-01-26 21:39:02 +0100

    Fixed conflict

--- dev/app/components/contact/formController.js
+++ dev/app/components/contact/formController.js
@@ -1,16 +1,12 @@
-<<<<<<< HEAD
 agorasturiasApp.controller('FormCtrl', ['$scope', 'Data'
 function ($scope, Data) {
-=======
-agorasturiasApp.controller('FormCtrl', ['$scope', '$http', function ($scope, $http) {
->>>>>>> 7b31582bc7fa80a2d0faac9cd3831bf1fe75a0f8
 
     $scope.submitForm = function(isValid,contact) {
 
         $scope.submitted = true;
 
         if (isValid) {
-<<<<<<< HEAD
+
             Data.push('mail',contact)
             .then(response){
               if(response.status==="success"){
@@ -24,23 +20,3 @@ agorasturiasApp.controller('FormCtrl', ['$scope', '$http', function ($scope, $ht
 
     };
 }]);
-=======
-            
-            $http.post('/mail', 
-
-                { email: $scope.email, message: $scope.message })
-
-                .success(function(data, status, headers, config) {
-                    // this callback will be called asynchronously
-                    // when the response is available
-                })
-                .error(function(data, status, headers, config) {
-                    // called asynchronously if an error occurs
-                    // or server returns response with an error status.
-                });
-            
-        }
-
-    };
-}]);
->>>>>>> 7b31582bc7fa80a2d0faac9cd3831bf1fe75a0f8
