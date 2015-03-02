2e1d11a4a338397845b8f805ef5f38f5fb5a838f
albertocuesta <albertocuesta@gmail.com>
2015-03-02 21:29:30 +0100

    Added changepwd en fixed amil * api to change password and to hash a given string * fixed contact form to send emai properly to the correct address.

--- api/v1/authentication.php
+++ api/v1/authentication.php
@@ -16,6 +16,7 @@ $app->get('/session', function() {
 $app->post('/login', function() use ($app) {
   require_once 'passwordHash.php';
   $r = json_decode($app->request->getBody());
+
   verifyRequiredParams(array('username', 'password'),$r);
   $response = array();
   $db = new DbHandler();
@@ -54,9 +55,83 @@ $app->post('/login', function() use ($app) {
 
 $app->get('/logout', function() {
   $db = new DbHandler();
+  $response = array();
   $session = $db->destroySession();
   $response["status"] = "info";
   $response["message"] = "Logged out successfully";
   echoResponse(200, $response);
 });
+
+/*
+Method to change the password of a given user
+Needs:
++ username: Username of the user changing password
++ curr_pass: Current password
++ new_pass: New password
+*/
+$app->put('/changepwd',function() use($app){
+  $db = new DbHandler();
+  require_once 'passwordHash.php';
+
+  $session = $db->getSession();
+  $r = json_decode($app->request->getBody());
+  $response = array();
+
+  $username = $r->username;
+  $password = $r->curr_pass;
+  $new_password = $r->new_pass;
+
+  $user = $db->getOneRecord("select uid,username,password from users where username='$username'");
+
+  /* if the user changing the password exists
+  if password matches and the user is the same as the one in the session */
+  if (isset($user)) {
+    if(passwordHash::check_password($user['password'],$password)
+    && $username == $session['username']){
+
+      $new_password_hash = passwordHash::hash($new_password);
+
+      if($db->prepared_query("UPDATE users set password = ? where username = ?",
+      "ss", array($new_password_hash, $username)))
+      {
+        $response["status"] = "success";
+        $response["message"] = "Password changed successfully";
+        echoResponse (200, $response);
+      }
+      else{
+        $response["status"] = "error";
+        $response["message"] = "Error in DB updating the password";
+        echoResponse (500, $response);
+      }
+
+    }
+    else {
+      $response["status"] = "error";
+      $response["message"] = "Not authorized to do this operation";
+      echoResponse (401, $response);
+    }
+
+  }
+  else {
+    $response["status"] = "error";
+    $response["message"] = "Requesting user does not exist";
+    echoResponse (403, $response);
+  }
+});
+
+/*
+  Generates and returns the hash for a given string
+  Requires:
+  + password: string with the password to be hashed
+  Returns:
+  + String with the hash of the string provided
+*/
+$app->post('/pass', function() use($app) {
+  require_once 'passwordHash.php';
+
+  $r = json_decode($app->request->getBody());
+
+  echo passwordHash::hash($r->password);
+});
+
 ?>
--- api/v1/mail.php
+++ api/v1/mail.php
@@ -6,23 +6,25 @@
     $r = json_decode($app->request->getBody(), true);
     $response = array();
 
-    $mail_to_send_to = "incoming@agorasturias.org";
-    $your_feedbackmail = "noreply@agorasturias.org";
+    $mail_to_send_to = "incoming-l@agorasturias.org";
+    $feedbackmail = "noreply@agorasturias.org";
 
-    $email = $r['email'] ;
-    $message = $r['message'] ;
-    $headers = "From: $your_feedbackmail" . "\r\n" . "Reply-To: $email, incoming@agorasturias.org" . "\r\n" ;
-    $a = mail( $mail_to_send_to, "Feedback Form Results", $message, $headers );
+    $email = $r['email'];
+    $message = $r['message'];
+    $name = $r['name'];
+    $headers = "From: $feedbackmail" . "\r\n" . "Reply-To: $email, incoming@agorasturias.org" . "\r\n" ;
+    $a = mail( $mail_to_send_to, "Contact from web form - $name", $message, $headers );
     if ($a)
     {
       $response['status'] = "success";
       $response['message'] = "Email sent successfully";
+      echoResponse(200,$response);
     } else {
       $response['status'] = "error";
       $response['message'] = "An error occured while sending the email";
+      echoResponse(500,$response);
     }
-
-    echoResponse(200,$response);
+    
     return;
 
   });
